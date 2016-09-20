de5e544f8247ca0e656d506ffd81a6cb30e47017
eifu <etomita18@gmail.com>
2016-09-19 23:58:25 -0400

    prompt user for new input

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"github.com/eifu/gometric/gometric"
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main() {
-	a := [][4]uint{
-		[4]uint{1, 1, 1, 0},
-		[4]uint{0, 1, 0, 0},
-		[4]uint{0, 0, 0, 0},
-		[4]uint{0, 0, 0, 0},
+	// a := [][4]uint{
+	// 	[4]uint{1, 1, 1, 0},
+	// 	[4]uint{0, 1, 0, 0},
+	// 	[4]uint{0, 0, 0, 0},
+	// 	[4]uint{0, 0, 0, 0},
+	// }
+
+	var a [][4]uint
+	var aa [4]uint
+	reader := bufio.NewReader(os.Stdin)
+	for i:= 0;i < 4; i++{
+		guess, err := reader.ReadString('\n')
+		if err!=nil{
+			fmt.Println("aa")
+			return 
+		}
+
+		aa = [4]uint{0, 0, 0, 0}
+		for j := 0; j < 4;j ++{
+			if guess[j] == '1'{
+				aa[j] = 1
+			}
+		}
+		a = append(a, aa)
 	}
 
 	c := gometric.Count(a)
